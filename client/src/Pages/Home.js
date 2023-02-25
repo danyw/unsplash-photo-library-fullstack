1c50f9faf940b27265efc23f3d507ea4fadd5e0d
danyw <danielwiejak@gmail.com>
2023-02-25 10:50:43 +0000

    initial commit, search photos work, display photos work

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Photos from "../Components/Photos";
+import SearchButton from "../Components/SearchButton";
+
+
+const Home = () => {
+    return (
+        <div className="home">
+        <h2>Home</h2>
+        <SearchButton />
+        {/* <Photos /> */}
+
+        </div>
+    );
+    };
+
+export default Home;
