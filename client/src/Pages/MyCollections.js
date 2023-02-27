e21531ca3976e6ee019dbf83b8839f186f5255b5
danyw <danielwiejak@gmai.com>
2023-02-27 22:55:57 +0000

    add delete photo from library

--- client/src/Pages/MyCollections.js
+++ client/src/Pages/MyCollections.js
@@ -1,28 +1,30 @@
 import React, { useState, useEffect } from "react";
+import { deleteFromLibrary } from "../Components/Collections/deleteFromLibrary";
 
 const MyCollections = () => {
- const [photos, setPhotos] = useState([]);
-  
+  const [photos, setPhotos] = useState([]);
 
   useEffect(() => {
     fetch(`http://localhost:5001/userCollections`)
-    .then((res) => res.json())
-    .then((data) => {
-      setPhotos(data);
-      console.log(photos);
-    });
-    
+      .then((res) => res.json())
+      .then((data) => {
+        setPhotos(data);
+      });
   }, []);
 
-  
+  const handleDelete = (photoId) => {
+    deleteFromLibrary(photoId, setPhotos);
+  };
+
   return (
     <div>
       <h2>My Collections</h2>
       <ul>
         {photos.map((photo) => (
-          <li key={photo._id}>
+          <li key={photo.photoId}>
             <img src={photo.imageUrlSmall} alt={photo.title} />
-            <p>{photo.title}</p>
+            <button onClick={() => handleDelete(photo.photoId)}>X</button>
+            <p>{photo.creatorsName}</p>
           </li>
         ))}
       </ul>
