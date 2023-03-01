1edf274f8f87fca3e424b042ee123feac66fbb21
danyw <danielwiejak@gmai.com>
2023-03-01 21:49:01 +0000

    replaced material ui with tailwind

--- client/src/Components/Photos/Photos.js
+++ client/src/Components/Photos/Photos.js
@@ -1,9 +1,7 @@
 import { useState, useEffect } from "react";
 // import AddToCollection from "./addToCollection";
 import { addToLibraryHandler } from "./addToLibraryHandler";
-import Box from "@mui/material/Box";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
+
 
 
 
@@ -25,36 +23,18 @@ const Photos = ({ title, setSearchClicked, searchClicked }) => {
 
 
   return (
-    // <div className=" photosContainer flex flex-row flex-wrap justify-center">
-    //   {typeof backendData === "undefined" ? (
-    //     <p>loading...</p>
-    //   ) : (
-    //     backendData.map((data, idx) => {
-    //       return (
-    //         <div key={idx}>
-    //           <img src={data.imageUrlSmall} alt={data.description} className="max-w-sm h-auto hover:drop-shadow-2xl" />
-    //           <button onClick={() => addToLibraryHandler(data)}>Add</button>
-              
-    //         </div>
-    //       );
-          
-    //     })
 
-
-    //   )}
-    // </div>
-
-<div className="photosContainer">
+<div className="columns-1 md:columns-2 lg:columns-3 gap-4 w-full md:w-11/12  lg:w-11/12 m-auto">
     
-      <Box sx={{ width: 1, height: 1, overflowY: "scroll" }}>
-        <ImageList variant="masonry" cols={3} gap={8}>
+      
           {backendData.map((photo) => (
-            <ImageListItem key={photo.photoId}>
+            <div key={photo.photoId} className="relative">
               <img
                 src={`${photo.imageUrlSmall}?w=248&fit=crop&auto=format`}
                 srcSet={`${photo.imageUrlSmall}?w=248&fit=crop&auto=format&dpr=2 2x`}
                 alt={photo.title}
                 loading="lazy"
+                className="mb-4"
               />
               <div className="absolute bottom-0 left-0 right-0  p-2 bg-slate-600 flex justify-between items-center opacity-0 hover:opacity-100">
                 <p className="text-white font-serif text-sm m-0 ">
@@ -67,10 +47,10 @@ const Photos = ({ title, setSearchClicked, searchClicked }) => {
                   Add
                 </button>
               </div>
-            </ImageListItem>
+            </div>
           ))}
-        </ImageList>
-      </Box>
+        
+     
     </div>
 
   );
